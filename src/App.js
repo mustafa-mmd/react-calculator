022da81061f04fb83ee9f428a5bea628ee3cd134
mustafajam <mustafajamalshabqadar@gmail.com>
2025-04-24 16:14:53 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calculator from './Calculator'
+import './App.css';
+function app() {
+  return (
+    <>
+    <h1>welcome to calculator project created by React</h1>
+    <Calculator></Calculator>
+    </>
+  )
+}
+
+export default app
