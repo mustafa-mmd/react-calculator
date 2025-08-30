995e752c3d96492ba95828504641955ffce2d588
mustafajam <mustafajamalshabqadar@gmail.com>
2025-08-30 16:57:46 +0500

    commit h1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function app() {
   return (
     <>
-    <h1>welcome to calculator project created by React</h1>
+    {/* <h1>welcome to calculator project created by React</h1> */}
     <Calculator></Calculator>
     </>
   )
